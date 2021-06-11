c2338949c60523ea4f5609520065d20048690853
Redoy <mdfuadulislamhridoy52@gmail.com>
2021-06-11 11:21:53 +0600

    Course Start

---
+++ 1-IntroductionToC++/c++Code.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1, num2;
+    cout<<"Enter two numbers: ";
+    cin>>num1>>num2;
+
+    int sum = num1+num2;
+    cout<<"Sum of two numbers is : "<<sum;
+    return 0;
+}
---
+++ 1-IntroductionToC++/cCode.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+    int num1, num2, sum;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&num1,&num2);
+
+    sum = num1 + num2;
+    printf("Sum of two numbers is : %d",sum);
+}
