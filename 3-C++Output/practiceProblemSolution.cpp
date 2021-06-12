16bc5875210df0a7d30c9676d242c03be81440c1
Redoy <mdfuadulislamhridoy52@gmail.com>
2021-06-13 00:02:33 +0600

    C++ Output Practice problem solution.

---
+++ 3-C++Output/practiceProblemSolution.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    cout<<"Name: MD Fuadul Islam Redoy"<<endl;
+    cout<<"Id: 2010"<<endl;
+    cout<<"Department: BCSE"<<endl;
+    cout<<"University: International University of Business Agriculture and Technology (IUBAT)";
+    
+}
