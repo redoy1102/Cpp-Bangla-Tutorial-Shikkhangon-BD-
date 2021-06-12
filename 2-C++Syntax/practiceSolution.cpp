93ab4891b18592bde782a60e1796a36c8559ef14
Redoy <mdfuadulislamhridoy52@gmail.com>
2021-06-12 23:53:25 +0600

    Syntax practice problem solution

---
+++ 2-C++Syntax/practiceSolution.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    cout<<"Name: Md Fuadul Islam Redoy";
+    cout<<"Father's Name: Md Harun Or Rashid";
+    cout<<"Mother's Name: Farida Begum";
+
+    return 0;
+}
