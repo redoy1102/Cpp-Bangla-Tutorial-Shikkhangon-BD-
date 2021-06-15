05607c1feec06a4cbdfd9a2bd495f6496c84ed23
Redoy <mdfuadulislamhridoy52@gmail.com>
2021-06-15 11:47:41 +0600

    Solution of variables practice problem done

---
+++ 5-C++Variables/practiceSolution.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1 = 20, num2 = 10;
+
+    cout<<"Summation of num1 and num2 = "<<num1 + num2<<endl;
+    cout<<"Subtraction of num1 and num2 = "<<num1 - num2<<endl;
+    cout<<"Multiplication of num1 and num2 = "<<num1 * num2<<endl;
+
+    return 0;
+}
