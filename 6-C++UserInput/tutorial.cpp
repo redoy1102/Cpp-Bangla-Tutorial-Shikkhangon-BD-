3a0c0bc584ea72d1ce8770fa3586757cd2765c3e
Redoy <mdfuadulislamhridoy52@gmail.com>
2021-06-16 12:12:35 +0600

    User input part finished

---
+++ 6-C++UserInput/tutorial.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int num1, num2;
+
+    cout<<"Enter two numbers: ";
+    cin>>num1>>num2;
+
+    cout<<"Number 1 = "<<num1<<endl;
+    cout<<"Number 2 = "<<num2;
+
+    return 0;
+}
