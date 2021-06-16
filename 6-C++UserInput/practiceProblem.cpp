e058ecc099736d56048c3db0b97d8b5e8055ab08
Redoy <mdfuadulislamhridoy52@gmail.com>
2021-06-16 11:17:31 +0600

    Practice problem solution of c++ user input

---
+++ 6-C++UserInput/practiceProblem.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1, num2;
+    
+    cout<<"Enter two number: ";
+    cin>>num1>>num2;
+
+    int sum = num1 + num2;
+
+    cout<<"Sum of "<<num1<< " and " <<num2<<" = "<<sum;
+
+    return 0;
+}
