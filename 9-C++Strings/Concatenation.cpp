2f88d8b5c30858622ad9063b832a8ae9ca408813
Redoy <mdfuadulislamhridoy52@gmail.com>
2021-06-18 17:16:58 +0600

    Data types part complete

---
+++ 9-C++Strings/Concatenation.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string firstName = "John";
+    string lastName = "Doe";
+
+    string fullName = firstName + lastName; //concatenation
+
+    cout << fullName;
+}
---
+++ 9-C++Strings/NumbersAndStrings.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+
+int main(){
+    int x = 10, y = 20;
+    
+
+
+
+
+
+    return 0;
+}
