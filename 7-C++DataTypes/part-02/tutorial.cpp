a8da49d251e8ea10fdddc307a826e53b03748e7d
Redoy <mdfuadulislamhridoy52@gmail.com>
2021-06-18 14:39:52 +0600

    Complete data types part

---
+++ 7-C++DataTypes/part-02/tutorial.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //Numbers part:
+
+    //Int:
+    int myNum = 1000;
+    cout<<myNum<<endl;
+
+    //float:
+    float myNum = 5.75;
+    cout << myNum<<endl;
+
+    //double:
+    double myNum = 5.75;
+    cout<<myNum<<endl;
+
+
+    //Booleans part
+    bool isCodingFun = true;
+    bool isFishTasty = false;
+
+    cout<<isCodingFun<<endl;
+    cout<<isFishTasty;
+
+    return 0;
+
+}
---
+++ 7-C++DataTypes/part-03/tutorial.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+
+    //characters part--
+    char myGrade = 'B';
+    cout<<myGrade<<endl;
+
+    char a = 65, b = 66, c = 67;
+    cout<<a;
+    cout<<b;
+    cout<<c<<endl;
+
+    //string part
+    string greeting = "Hello";
+    cout<<greeting;
+}
